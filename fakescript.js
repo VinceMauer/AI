fc0a1c64a8e2252a6ea536c3ab594b2478b41a7c
Agunovvski <33866004+Agunovvski@users.noreply.github.com>
2018-10-18 20:31:19 +0200

    Klaar, alleen nog styling

--- fakescript.js
+++ fakescript.js
@@ -2,6 +2,7 @@ var age = document.querySelector("h2.age");
 var gender = document.querySelector("h2.gender");
 var emotionmax = document.querySelector("h2.emotion-max");
 var selectCover = document.querySelector('img.cover');
+var suggestButton = document.querySelector('.suggest');
 
 var titleofmovie = document.querySelector(".movie-title");
 var descriptionofmovie = document.querySelector(".movie-description");
@@ -132,17 +133,17 @@ function processImage() {
                 var sadnessMoviesArray = [jsonObj[4], jsonObj[0], jsonObj[5]];
 
                 var film1 = {cover:"https://fesapusewebsite.blob.core.windows.net/fathom/castleinthesky-1000x1480-r3-f6aba65dfc41c4fc47fc9289c1674057.jpg"};
-                // var film2 = {mood:"anger", age:"18", gender:"male"};
-                // var film3 = {mood:"contempt", age:"18", gender:"female"};
-                // var film4 = {mood:"happiness", age:"18", gender:"male"};
-                // var film5 = {mood:"sadness", age:"18", gender:"female"};
-                // var film6 = {mood:"disgust", age:"18", gender:"male"};
-                // var film7 = {mood:"anger", age:"18", gender:"female"};
-                // var film8 = {mood:"contempt", age:"18", gender:"male"};
-                // var film9 = {mood:"anger", age:"18", gender:"male"};
-                // var film10 = {mood:"neutral", age:"18", gender:"male"};
-                // var film11 = {mood:"neutral", age:"0", gender:"female"};
-                // var film12 = {mood:"hapiness", age:"0", gender:"male"};
+                var film2 = {cover:"http://www.gablescinema.com/media/filmassets/Grave_of_the_Fireflies_Poster.jpg.500x715_q85_crop-smart.jpg"};
+                var film3 = {cover:"https://fesapusewebsite.blob.core.windows.net/fathom/totoro-eventposter-24729b7943d6fce9198fcb8af5b602b3.jpg"};
+                var film4 = {cover:"https://i.pinimg.com/originals/9f/b5/a4/9fb5a43fe303b14e140ca4276d28091e.jpg"};
+                var film5 = {cover:"https://d3fa68hw0m2vcc.cloudfront.net/814/102017248.jpeg"};
+                var film6 = {cover:"https://uvmbored.com/wp-content/uploads/2018/05/porcorosso-1000x1480-r2-4f67caff60f8deda5ff0bf39bc430f67.jpg"};
+                var film7 = {cover:"https://s.s-bol.com/imgbase0/imagebase3/large/FC/5/0/0/7/1002004004607005.jpg"};
+                var film8 = {cover:"https://images-na.ssl-images-amazon.com/images/I/91k9zjN55iL._SY445_.jpg"};
+                var film9 = {cover:"https://fesapusewebsite.blob.core.windows.net/fathom/princessmononoke-1000x1480-r5-468f9dd25a9f87a92d72c6c1ac3ca1ff.jpg"};
+                var film10 = {cover:"https://vignette.wikia.nocookie.net/dubbing9585/images/f/fa/My-neighbors-the-yamadas-poster.jpg/revision/latest?cb=20171223052252"};
+                var film11 = {cover:"https://fesapusewebsite.blob.core.windows.net/fathom/spiritedaway-1000x1480-r2-81ce56a4d406c5cc5bf5a9bb50a2772b.jpg"};
+                var film12 = {cover:"https://prodimage.images-bn.com/pimages/0826663181593_p0_v1_s550x406.jpg"};
                 //
                 function merge(objSrc, objTarget){
                  return Object.keys(objTarget).reduce(function(src, prop){
@@ -152,18 +153,18 @@ function processImage() {
                 }
                 //
                 merge(jsonObj[0], film1);
-                // merge(jsonObj[1], film2);
-                // merge(jsonObj[2], film3);
-                // merge(jsonObj[3], film4);
-                // merge(jsonObj[4], film5);
-                // merge(jsonObj[5], film6);
-                // merge(jsonObj[6], film7);
-                // merge(jsonObj[7], film8);
-                // merge(jsonObj[8], film9);
-                // merge(jsonObj[9], film10);
-                // merge(jsonObj[10], film11);
-                // merge(jsonObj[11], film12);
-                //
+                merge(jsonObj[1], film2);
+                merge(jsonObj[2], film3);
+                merge(jsonObj[3], film4);
+                merge(jsonObj[4], film5);
+                merge(jsonObj[5], film6);
+                merge(jsonObj[6], film7);
+                merge(jsonObj[7], film8);
+                merge(jsonObj[8], film9);
+                merge(jsonObj[9], film10);
+                merge(jsonObj[10], film11);
+                merge(jsonObj[11], film12);
+
                 // console.log(jsonObj[3]);
 
 
@@ -179,6 +180,7 @@ function processImage() {
                   releasedateofmovie.textContent = randomNeutral["release_date"];
                   // loadMore.setAttribute('class', 'more');
                   selectCover.src = randomNeutral["cover"];
+                  suggestButton.style.display = "block";
                 }  else if (emotionmax.textContent == "anger") {
                   // console.log("angry bitch");
                   document.querySelector("body").style.color = "red";
@@ -189,14 +191,16 @@ function processImage() {
                   descriptionofmovie.textContent = randomAnger["description"];
                   directorofmovie.textContent = randomAnger["director"];
                   releasedateofmovie.textContent = randomAnger["release_date"];
+                  selectCover.src = randomAnger["cover"];
                 } else if (emotionmax.textContent == "happiness") {
-                  // console.log("happy bitch");
+                  console.log("happy bitch");
                   var randomHappiness = happinessMoviesArray[Math.floor(happinessMoviesArray.length * Math.random())];
 
                   titleofmovie.textContent = randomHappiness["title"];
                   descriptionofmovie.textContent = randomHappiness["description"];
                   directorofmovie.textContent = randomHappiness["director"];
                   releasedateofmovie.textContent = randomHappiness["release_date"];
+                  selectCover.src = randomHappiness["cover"];
                 } else if (emotionmax.textContent == "contempt") {
                   // console.log("contempt bitch");
                   var randomContempt = contemptMoviesArray[Math.floor(contemptMoviesArray.length * Math.random())];
@@ -205,6 +209,7 @@ function processImage() {
                   descriptionofmovie.textContent = randomContempt["description"];
                   directorofmovie.textContent = randomContempt["director"];
                   releasedateofmovie.textContent = randomContempt["release_date"];
+                  selectCover.src = randomContempt["cover"];
                 } else if (emotionmax.textContent == "disgust") {
                   // console.log("disgust bitch");
                   var randomDisgust = disgustMoviesArray[Math.floor(disgustMoviesArray.length * Math.random())];
@@ -213,6 +218,7 @@ function processImage() {
                   descriptionofmovie.textContent = randomDisgust["description"];
                   directorofmovie.textContent = randomDisgust["director"];
                   releasedateofmovie.textContent = randomDisgust["release_date"];
+                  selectCover.src = randomDisgust["cover"];
                 } else if (emotionmax.textContent == "sadness") {
                   // console.log("sadness bitch");
                   var randomSadness = sadnessMoviesArray[Math.floor(sadnessMoviesArray.length * Math.random())];
@@ -221,6 +227,7 @@ function processImage() {
                   descriptionofmovie.textContent = randomSadness["description"];
                   directorofmovie.textContent = randomSadness["director"];
                   releasedateofmovie.textContent = randomSadness["release_date"];
+                  selectCover.src = randomSadness["cover"];
                 }
 
                 };
